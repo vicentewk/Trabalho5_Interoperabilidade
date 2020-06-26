1b2d4166ebb84f203a86b1cda031cfff2bf01f2e
Vicente Kryszczun <vicentewk@gmail.com>
2020-06-26 19:09:17 -0300

    criação de arquivo json em python adicionado

---
+++ json_python.py
@@ -0,0 +1,11 @@
+import json
+
+dados_gravados={
+    "nomes":["Vicente","Kassuelo"],
+    "curso":"Ciencia da Computacao",
+    "professor":"Edson Padoin"
+
+}
+
+with open('dados_gravados.json','w') as json_file:
+        json.dump(dados_gravados,json_file,indent=4,sort_keys=False)
